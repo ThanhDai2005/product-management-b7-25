0341f1cf485bb1fa6dbb086c24960b41c21457d0
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-07-17 00:34:10 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3002;
+require("dotenv").config();
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
