96e5ab66281ce4707e70ec7a0dfbcc015002e30b
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-08-02 15:26:30 +0700

    3.2. Thay đổi trạng thái nhiều sản phẩm

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -58,3 +58,73 @@ if (buttonPagination) {
 }
 
 // End Pagination
+
+// Checkbox Multi
+
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+
+if (checkboxMulti) {
+  const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+  const inputId = checkboxMulti.querySelectorAll("input[name='id']");
+
+  inputCheckAll.addEventListener("click", () => {
+    console.log(inputCheckAll.checked);
+    if (inputCheckAll.checked) {
+      inputId.forEach((input) => {
+        input.checked = true;
+      });
+    } else {
+      inputId.forEach((input) => {
+        input.checked = false;
+      });
+    }
+  });
+
+  inputId.forEach((input) => {
+    input.addEventListener("click", () => {
+      const countChecked = checkboxMulti.querySelectorAll(
+        "input[name='id']:checked"
+      ).length;
+
+      if (countChecked == inputId.length) {
+        inputCheckAll.checked = true;
+      } else {
+        inputCheckAll.checked = false;
+      }
+    });
+  });
+}
+
+// End Checkbox Multi
+
+// Form Change Multi
+
+const formChangeMulti = document.querySelector("[form-change-multi]");
+
+if (formChangeMulti) {
+  formChangeMulti.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const checkboxMulti = document.querySelector("[checkbox-multi]");
+    const inputChecked = checkboxMulti.querySelectorAll(
+      "input[name='id']:checked"
+    );
+
+    if (inputChecked.length > 0) {
+      let ids = [];
+      const inputIds = formChangeMulti.querySelector("input[name='ids']");
+
+      inputChecked.forEach((input) => {
+        const id = input.value;
+        ids.push(id);
+      });
+
+      inputIds.value = ids.join(", ");
+
+      formChangeMulti.submit();
+    } else {
+      alert("Vui lòng chọn ít nhất một bản ghi");
+    }
+  });
+}
+
+// End Form Change Multi
