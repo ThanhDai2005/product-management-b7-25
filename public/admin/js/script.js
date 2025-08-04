a995e40a756f969a2dfd127c30745c08be8e7ecd
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-08-04 12:35:26 +0700

    1. Tính năng xóa nhiều sản phẩm

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -109,6 +109,16 @@ if (formChangeMulti) {
       "input[name='id']:checked"
     );
 
+    const typeChange = e.target[0].value;
+
+    if (typeChange == "delete-all") {
+      const isConfirm = confirm("Bạn có chắc muốn xóa những sản phẩm này?");
+
+      if (!isConfirm) {
+        return;
+      }
+    }
+
     if (inputChecked.length > 0) {
       let ids = [];
       const inputIds = formChangeMulti.querySelector("input[name='ids']");
