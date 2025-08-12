cf6c1563ebaa6c44635dc14b9cac614531b7187a
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-08-13 00:03:28 +0700

    6. Đẩy file tĩnh lên Cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -160,10 +160,6 @@ module.exports.createPost = async (req, res) => {
     req.body.position = parseInt(req.body.position);
   }
 
-  if (req.file) {
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
-  }
-
   const product = new Product(req.body);
   await product.save();
 
@@ -198,9 +194,6 @@ module.exports.editPatch = async (req, res) => {
   req.body.stock = parseInt(req.body.stock);
   req.body.position = parseInt(req.body.position);
 
-  if (req.file) {
-    req.body.thumbnail = `/uploads/${req.file.filename}`;
-  }
   try {
     await Product.updateOne({ _id: id }, req.body);
     req.flash("success", `Cập nhật thành công!`);
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter");
-const upload = multer({ storage: storageMulter() });
+
+const upload = multer();
 
 const controller = require("../../controllers/admin/product.controller");
 
 const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 
@@ -21,6 +23,7 @@ router.get("/create", controller.create);
 router.post(
   "/create",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.createPost
 );
@@ -30,6 +33,7 @@ router.get("/edit/:id", controller.edit);
 router.patch(
   "/edit/:id",
   upload.single("thumbnail"),
+  uploadCloud.upload,
   validate.createPost,
   controller.editPatch
 );
