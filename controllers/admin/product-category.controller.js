cece0e5ef535fcaddf95f13ee591671ef00f983c
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-08-18 16:10:38 +0700

    3. Xây dựng phần quản lý danh mục sản phẩm (Phần 2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -2,6 +2,8 @@ const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree");
+
 // [GET] /admin/product-category
 module.exports.index = async (req, res) => {
   let find = {
@@ -10,16 +12,27 @@ module.exports.index = async (req, res) => {
 
   const records = await ProductCategory.find(find);
 
+  const newRecords = createTreeHelper.tree(records);
+
   res.render("admin/pages/product-category/index", {
     pageTitle: "Danh mục sản phẩm",
-    records: records,
+    records: newRecords,
   });
 };
 
 // [GET] /admin/product-category/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
+
   res.render("admin/pages/product-category/create", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords,
   });
 };
 
