1ddf49d826221119003a0088cf0435a790b7ed1b
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-08-19 13:06:46 +0700

    1. Tính năng chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -51,3 +51,40 @@ module.exports.createPost = async (req, res) => {
 
   res.redirect(`${systemConfig.prefixAdmin}/product-category`);
 };
+
+// [GET] /admin/product-category/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let find = {
+      deleted: false,
+      _id: id,
+    };
+
+    const data = await ProductCategory.findOne(find);
+
+    const records = await ProductCategory.find({ deleted: false });
+
+    const newRecords = createTreeHelper.tree(records);
+
+    res.render("admin/pages/product-category/edit", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords,
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/product-category`);
+  }
+};
+
+// [PATCH] /admin/product-category/edit/:id
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+
+  req.body.position = parseInt(req.body.position);
+
+  await ProductCategory.updateOne({ _id: id }, req.body);
+
+  const previousUrl = req.get("referer");
+  res.redirect(previousUrl);
+};
