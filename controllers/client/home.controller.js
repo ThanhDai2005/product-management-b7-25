17421b9c33880df619a0472b5b7ecd1119893d2e
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-08-29 16:48:28 +0700

    4.1. Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,22 @@
+const Product = require("../../models/product.model");
+
+const productHelper = require("../../helpers/product");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  // lấy ra sản phẩm nổi bật
+  const productFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active",
+  }).limit(6);
+
+  const newProducts = productHelper.priceNewProducts(productFeatured);
+
+  // Hết lấy ra sản phẩm nổi bật
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang Chủ",
+    productsFeatured: newProducts,
   });
 };
