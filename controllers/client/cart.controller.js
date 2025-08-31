b4edc7da4cff35932d1e3291908611ac7743e1c1
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-09-01 00:34:12 +0700

    2.1. Thêm sản phẩm vào giỏ hàng

---
+++ controllers/client/cart.controller.js
@@ -0,0 +1,49 @@
+const Cart = require("../../models/cart.model");
+
+// [POST] /cart/add/:productId
+module.exports.addPost = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  const quantity = parseInt(req.body.quantity);
+
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+
+  const existProductInCart = cart.products.find(
+    (item) => item.product_id == productId
+  );
+
+  if (existProductInCart) {
+    const newQuantity = existProductInCart.quantity + quantity;
+
+    await Cart.updateOne(
+      {
+        _id: cartId,
+        "products.product_i": productId,
+      },
+      {
+        "products.$.quantity": newQuantity,
+      }
+    );
+  } else {
+    const objectCart = {
+      product_id: productId,
+      quantity: quantity,
+    };
+
+    await Cart.updateOne(
+      {
+        _id: cartId,
+      },
+      {
+        $push: { products: objectCart },
+      }
+    );
+  }
+
+  req.flash("success", "Thêm sản phẩm vào giỏ hàng thành công!");
+
+  const previousUrl = req.get("referer");
+  res.redirect(previousUrl);
+};
