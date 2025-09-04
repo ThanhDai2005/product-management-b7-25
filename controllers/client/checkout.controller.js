00244c97bebe381fe2271eb0765b69206368302b
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-09-04 18:19:53 +0700

    1.4. Làm trang đặt hàng thành công

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -61,7 +61,7 @@ module.exports.order = async (req, res) => {
       _id: product.product_id,
     });
 
-    objectProduct.price = productsHelper.priceNewProduct(productInfo);
+    objectProduct.price = productInfo.price;
     objectProduct.discountPercentage = productInfo.discountPercentage;
 
     products.push(objectProduct);
@@ -87,4 +87,33 @@ module.exports.order = async (req, res) => {
   );
 
   res.redirect(`/checkout/success/${order.id}`);
+};
+
+// [POST] /checkout/success/:orderId
+module.exports.success = async (req, res) => {
+  const order = await Order.findOne({
+    _id: req.params.orderId,
+  });
+
+  for (const product of order.products) {
+    const productInfo = await Product.findOne({
+      _id: product.product_id,
+    }).select("title thumbnail");
+
+    product.productInfo = productInfo;
+
+    product.priceNew = productsHelper.priceNewProduct(product);
+
+    product.totalPrice = product.priceNew * product.quantity;
+  }
+
+  order.totalPrice = order.products.reduce(
+    (sum, item) => sum + item.totalPrice,
+    0
+  );
+  console.log(order);
+  res.render("client/pages/checkout/success", {
+    pageTitle: "Đặt hàng thành công",
+    order: order,
+  });
 };
