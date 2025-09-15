2390b7e7353851014df9a701a39c361d5db9e60d
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-09-16 00:12:04 +0700

    5.3. Hiển thị data realtime

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,4 @@
 // CLIENT_SEND_MESSAGE
-
 const formSendData = document.querySelector(".chat .inner-form");
 
 if (formSendData) {
@@ -12,5 +11,29 @@ if (formSendData) {
     }
   });
 }
-
 //End CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+  const body = document.querySelector(".chat .inner-body");
+
+  const div = document.createElement("div");
+
+  let htmlFullName = "";
+
+  if (myId == data.userId) {
+    div.classList.add("inner-outgoing");
+  } else {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  }
+
+  div.innerHTML = `
+  ${htmlFullName}
+  <div class="inner-content">${data.content}</div>
+  `;
+
+  body.appendChild(div);
+});
+// END SERVER_RETURN_MESSAGE
