63989f1de86bcd6228c609483c06128e70c041fc
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-09-15 16:13:39 +0700

    5.2. Làm tính năng chat cơ bản

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,16 @@
-var socket = io();
+// CLIENT_SEND_MESSAGE
+
+const formSendData = document.querySelector(".chat .inner-form");
+
+if (formSendData) {
+  formSendData.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const content = e.target[0].value;
+    if (content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      e.target[0].value = "";
+    }
+  });
+}
+
+//End CLIENT_SEND_MESSAGE
