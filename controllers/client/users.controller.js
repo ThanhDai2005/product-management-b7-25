4f447ab03b3c08b48a150dfdb82b5e3e6b3e7147
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-09-28 00:18:28 +0700

    1.3. Chức năng gửi yêu cầu

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -1,16 +1,34 @@
 const User = require("../../models/user.model");
 
+const userSocket = require("../../sockets/client/users.socket");
+
 // [GET] /not-friend
 module.exports.notFriend = async (req, res) => {
+  // Socket
+  userSocket(res);
+  // End Socket
+
   const userId = res.locals.user.id;
 
+  const myUser = await User.findOne({
+    _id: userId,
+  });
+
+  const requestFriends = myUser.requestFriends;
+  const acceptFriends = myUser.acceptFriends;
+
+  console.log(requestFriends);
+
   const users = await User.find({
-    _id: { $ne: userId },
+    $and: [
+      { _id: { $ne: userId } },
+      { _id: { $nin: requestFriends } },
+      { _id: { $nin: acceptFriends } },
+    ],
+
     status: "active",
     deleted: false,
   }).select("avatar fullName");
-
-  console.log(users);
 
   res.render("client/pages/users/not-friend", {
     pageTitle: "Danh sách người dùng",
