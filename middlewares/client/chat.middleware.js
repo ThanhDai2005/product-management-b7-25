e61ad3e8b3457f1225f0903463086df5d4241f7e
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-10-04 17:46:07 +0700

    1.3. Hiển thị danh sách tin nhắn  theo phòng chat

---
+++ middlewares/client/chat.middleware.js
@@ -0,0 +1,23 @@
+const User = require("../../models/user.model");
+const RoomChat = require("../../models/rooms-chat.model");
+
+module.exports.isAccess = async (req, res, next) => {
+  const userId = res.locals.user.id;
+  const roomChatId = req.params.roomChatId;
+
+  try {
+    const isAccessRoomChat = await RoomChat.findOne({
+      _id: roomChatId,
+      "users.user_id": userId,
+      deleted: false,
+    });
+
+    if (isAccessRoomChat) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  } catch (error) {
+    res.redirect("/");
+  }
+};
