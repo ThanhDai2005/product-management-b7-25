cd4def7cc151aaea6e684c771537f149a0f4a12c
Nguyen Van Thanh Dai <Dai2272005nv@gmail.com>
2025-10-06 01:29:20 +0700

    2.2. Tạo phòng chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -1,6 +1,56 @@
+const User = require("../../models/user.model");
+const RoomChat = require("../../models/rooms-chat.model");
+
 // [GET] /rooms-chat
 module.exports.index = async (req, res) => {
   res.render("client/pages/rooms-chat/index", {
     pageTitle: "Danh sách phòng",
   });
+};
+
+// [GET] /rooms-chat/create
+module.exports.create = async (req, res) => {
+  const FriendList = res.locals.user.FriendList;
+
+  for (const friend of FriendList) {
+    const infoFriend = await User.findOne({
+      _id: friend.user_id,
+    }).select("fullName avatar");
+
+    friend.infoFriend = infoFriend;
+  }
+
+  res.render("client/pages/rooms-chat/create", {
+    pageTitle: "Tạo phòng",
+    FriendList: FriendList,
+  });
+};
+
+// [POST] /rooms-chat/create
+module.exports.createPost = async (req, res) => {
+  const title = req.body.title;
+  const usersId = req.body.usersId;
+
+  const dataChat = {
+    title: title,
+    typeRoom: "group",
+    users: [],
+  };
+
+  usersId.forEach((userId) => {
+    dataChat.users.push({
+      user_id: userId,
+      role: "user",
+    });
+  });
+
+  dataChat.users.push({
+    user_id: res.locals.user.id,
+    role: "superAdmin",
+  });
+
+  const room = new RoomChat(dataChat);
+  await room.save();
+
+  res.redirect(`/chat/${room.id}`);
 };
